c20a234e9b1b3c864338190caed3ae7bbb32032d
mhucka <mhucka@google.com>
2025-01-15 06:15:14 +0000

    foo

--- parse_context.cc
+++ parse_context.cc
@@ -68,7 +68,6 @@ Status ParsePrograms(OpKernelContext* context, const std::string& input_name,
     return status;  }
 
   if (input->dims() != 1) 
-
 {
     // Never parse anything other than a 1d list of circuits.
     return Status(
