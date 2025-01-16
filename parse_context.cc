d844a76752f3d8c73397d833c325d3c388927906
mhucka <mhucka@google.com>
2025-01-16 01:14:14 +0000

    wip

--- parse_context.cc
+++ parse_context.cc
@@ -44,10 +44,7 @@ Status ParseProto(const std::string& text, T* proto) {
   // First attempt to parse from the binary representation.
 
   if (proto->ParseFromString(text))
-
-
-
-
+    
 
   {
     return ::tensorflow::Status(); 
