de46b0be547183e22003c4c963bb49e6ac62a22b
mhucka <mhucka@google.com>
2025-07-05 11:51:16 -0700

    wip

--- check_avx2.cc
+++ check_avx2.cc
@@ -4,5 +4,6 @@ int main() {
     // Get registers EAX=7 and ECX. (C.f. https://en.wikipedia.org/wiki/CPUID).
     __cpuidex(cpuInfo, 7, 0);
     // AVX2 is indicated by bit 5 of the EBX register (cpuInfo[1]).
-    (cpuInfo[1] & (1 << 5)) ? return 0 : return 1;
+    // Per Unix convention, a return value of 0 is success.
+    return (cpuInfo[1] & (1 << 5)) ? 0 : 1;
 }
